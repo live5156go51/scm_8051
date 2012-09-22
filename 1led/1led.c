4e32a47a67fdb8ac848fecbfcb7c6c9ce0b125ed
junstrix <junstrix@gmail.com>
2012-09-22 09:52:04 +0800

    1led update complete version

--- 1led/1led.c
+++ 1led/1led.c
@@ -1,9 +1,6 @@
 #include <AT89X51.H>
 
 sbit L1=P1^0;
-sbit L2=P1^1;
-sbit L3=P1^2;
-sbit L4=P1^3;
 
 void delay02s(void)
 {
@@ -25,14 +22,5 @@ void main(void)
 		L1=0;
 		delay02s();
 		L1=1;
-		L2=0;
-		delay02s();
-		L2=1;
-		L3=0;
-		delay02s();
-		L3=1;
-		L4=0;
-		delay02s();
-		L4=1;
 	}
 }
